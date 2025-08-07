03878687d94b7e48f2c83e5ecf072c0b2a03ef00
Irfankhan132 <irfanee125@gmail.com>
2025-08-07 22:56:11 +0200

    for loop example

---
+++ src/components/ForLoop.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>For Loop</h1>
+</template>
+
+<script>
+
+export default{
+    name:"ForLoop",
+    data(){
+        return{
+
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+
+h1{
+    color: bisque;
+}
+</style>
